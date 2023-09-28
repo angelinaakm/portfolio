f8496d9db9a77844ecb9ac55f8c5eeb472c9ccf3
Angelina Kim <ang.km777@gmail.com>
2023-09-28 14:20:03 +0100

    added more to the script + another html page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const redbutton = document.querySelector('.redirect-button')
 
 navToggle.addEventListener('click', ()=>{
     document.body.classList.toggle('nav-open'); //add new class nav-open and add it to css file
@@ -9,4 +10,8 @@ navLinks.forEach(link =>{
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
+})
+
+redbutton.addEventListener('click', ()=>{
+    location.href = "portfolio-item.html"
 })
