bbf8e8c6dd930c3145a9247d60b69204751d0f32
Angelina Kim <ang.km777@gmail.com>
2023-09-28 13:45:20 +0100

    new html

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
 navToggle.addEventListener('click', ()=>{
     document.body.classList.toggle('nav-open'); //add new class nav-open and add it to css file
+})
+
+navLinks.forEach(link =>{
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
 })
